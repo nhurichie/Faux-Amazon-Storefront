06f7a22ce330e1b1aa8f7ef36c54a657c10168d2
nhurichie <richnhu@gmail.com>
2019-05-27 22:18:50 -0500

    added_gitignore_hidekeys

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,7 +11,7 @@ var Table = require("cli-table2");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "****",
+  password: "root",
   database: "bamazon_db",
   port: 8889
 });
@@ -57,11 +57,11 @@ var displayConnection = function () {
 //FUNCTION TO PROMPT:SELECT & PURCHASE
 //*************************************
 var shoppingCart = function () {
-  inquirer.prompt({
+  inquirer.prompt([{
     name: "productToPurchase",
     type: "input",
     message: "Enter Product ID to make a purchase!"
-  }).then(function (responseOne) {
+  }]).then(function (responseOne) {
     //*****************************
     //CONNECTING QUERY TO DATABASE
     //*****************************
@@ -77,14 +77,14 @@ var shoppingCart = function () {
 
         shoppingCart("Good Selection!");
       } else {
-        inquirer.prompt({
+        inquirer.prompt([{
           name: "quantity",
           type: "input",
           message: "How many items do you want to put in your cart?"
-        }).then(function (responseTwo) {
+        }]).then(function (responseTwo) {
 
           //********************************
-          //SELECT NEW PRODUCT LOW INVENTORY
+          //SELECT QUANTITY CALLBACK
           //********************************
           var quantity = responseTwo.quantity;
           if (quantity > res[0].stock_quantity) {
@@ -95,6 +95,9 @@ var shoppingCart = function () {
             console.log(res[0].product_name + " purchased");
             console.log(quantity + " qty @ $" + res[0].price);
 
+            //********************************
+            //UPDATE NEW INVENTORY +/-
+            //********************************
             var newItemQuantity = res[0].stock_quantity - quantity;
             connection.query(
               "UPDATE products SET stock_quantity = " + newItemQuantity + "WHERE id= " + res[0].id, function (err, resUpdate) {
